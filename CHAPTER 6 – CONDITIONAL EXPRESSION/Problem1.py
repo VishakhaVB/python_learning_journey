d39541c77172eb5b5905de243326e5cf7dc5b484
Vishakha Bhilwadkar <vishakhabhilwadkar@gmail.com>
2026-03-07 18:59:14 +0530

    Add new Python learning progress and practice code

--- CHAPTER 6 – CONDITIONAL EXPRESSION/Problem1.py
+++ CHAPTER 6 – CONDITIONAL EXPRESSION/Problem1.py
@@ -1,17 +1,17 @@
 # 1. Write a program to find the greatest of four numbers entered by the user.
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-num3 = float(input("Enter third number: "))
-num4 = float(input("Enter fourth number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+num4 = int(input("Enter fourth number: "))
 
 if num1 >= num2 and num1 >= num3 and num1 >= num4:
-    greatest = num1
+    print(f"The greatest number is: {num1}")
 elif num2 >= num1 and num2 >= num3 and num2 >= num4:
-    greatest = num2
+    print(f"The greatest number is: {num2}")
 elif num3 >= num1 and num3 >= num2 and num3 >= num4:
-    greatest = num3
+    print(f"The greatest number is: {num3}")
 else:
-    greatest = num4
+    print(f"The greatest number is: {num4}")
 
-print(f"The greatest number is: {greatest}")
+
